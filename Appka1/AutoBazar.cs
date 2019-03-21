1c15f172397a5af666912f520ef97c8d1bd9e3f2
JakubCelovsky <celovsky.jakubmail.com>
2019-03-21 17:25:38 +0100

    Finall version without comments

--- Appka1/AutoBazar.cs
+++ Appka1/AutoBazar.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace Appka1
@@ -15,6 +16,11 @@ namespace Appka1
 
         public void LoadFile()
         {
+            if (!File.Exists("bazos.txt"))
+            {
+                File.Create("bazos.txt").Close();
+            }
+           
             string line;
             using (StreamReader sr = new StreamReader("bazos.txt"))
             {
@@ -35,32 +41,15 @@ namespace Appka1
                         Condition = bool.Parse(loadFile[9])
                     });
                 }
-
             }
-
-
+            
+            
         }
+       
 
         public AutoBazar()
         {
             LoadFile();
-        }
-
-        public int ReadCorrectIntValue()
-        {
-
-            bool isOk = true;
-            int ret = 0;
-            while (isOk)
-            {
-                isOk = int.TryParse(Console.ReadLine(), out ret);
-                if (isOk)
-                {
-                    return ret;
-                }
-                Console.WriteLine("Zadaj číslo");
-            }
-            return ret;
         }
 
         public void Vypis()
@@ -68,27 +57,27 @@ namespace Appka1
             foreach (var item in carList)
             {
                 Console.WriteLine(item.DescribeMe());
-            }
-            Console.WriteLine(carList.Count);
+            }            
         }
 
-
-        public int GetNextId(List<Auto> carList)
-
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="carList"></param>
+        /// <returns></returns>
+        public int GetNextId(List<Auto> carList)            
         {
-            if (carList.Last().Id > 0)
+            if (carList.Count > 0)
             {
                 int lastId = carList.Last().Id;
                 lastId++;
                 return lastId;
-
             }
             else
             {
                 int lastId = 1;
                 return lastId;
             }
-
         }
 
         public void DeleteCar(int delCar)
@@ -101,11 +90,7 @@ namespace Appka1
                     SaveToFile();
                     break;
                 }
-
             }
-
-
-
         }
 
         public void UpdateCar(int updCar)
@@ -114,94 +99,136 @@ namespace Appka1
             {
                 if (carList[i].Id == updCar)
                 {
-                    Console.WriteLine("Zmena : 1- Značka, 2-Typ auta, 3- Typ paliva,4- Rok výroby ,5- Najazdené km, 6- Cena, 7- Počet dverí, 8- Miesto predaja, 9- Havarované");
+                    Console.WriteLine("Zmena : 1- Značka, 2-Typ auta, 3- Typ paliva,4- Rok výroby ,5- Najazdené km, 6- Cena, 7- Počet dverí, 8- Miesto predaja, 9- Havarované, 0-žiadna zmena");
                     int cUpd = ReadCorrectIntValue();
                     bool correctValue = true;
                     switch (cUpd)
                     {
                         case 1:
                             Console.WriteLine("Ideš zmeniť značku " + carList[i].Brand + " na:");
-                            carList[i].Brand = Console.ReadLine();
+                            carList[i].Brand = CorrectString();
                             SaveToFile();
                             break;
                         case 2:
                             Console.WriteLine("Ideš zmeniť typ auta " + carList[i].TypeOfCar + " na:");
-                            carList[i].TypeOfCar = Console.ReadLine();
+                            carList[i].TypeOfCar = CorrectString();
                             SaveToFile();
                             break;
                         case 3:
-                            Console.WriteLine("Ideš zmeniť typ paliva " + carList[i].Fuel + " na:");
-                            carList[i].Fuel = ((FuelType)Enum.Parse(typeof(FuelType), Console.ReadLine()));
+                            Console.WriteLine("Ideš zmeniť typ paliva " + carList[i].Fuel + " na:(B-benzin, N-nafta, P-plyn, E-elektricke alebo H-hybrid) ");
+                            carList[i].Fuel =CorrectFuel();
                             SaveToFile();
                             break;
                         case 4:
                             Console.WriteLine("Ideš zmeniť rok výroby " + carList[i].YearOfProd + " na:");
-
-                            while (correctValue)
-                            {
-                                int yOfProd = ReadCorrectIntValue();
-                                if (yOfProd > 1870 && yOfProd <= DateTime.Now.Year)
-                                {
-                                    carList[i].YearOfProd = yOfProd;
-                                    correctValue = false;
-                                }
-                                else { Console.WriteLine("Rok musí byť od 1870 až po aktuálny rok"); }
-                            }
+                            carList[i].YearOfProd = CorrectYear();
                             SaveToFile();
                             break;
                         case 5:
                             Console.WriteLine("Ideš zmeniť najazdené kilometre " + carList[i].MileAge + " na: (POZOR-je to protizákonné)");
-
-                            while (correctValue)
-                            {
-
-
-                                int mAge = ReadCorrectIntValue();
-                                if (mAge >= 0)
-                                {
-                                    carList[i].MileAge = mAge;
-                                    correctValue = false;
-                                }
-                                else { Console.WriteLine("Kilometre nemôžu byť mínusová hodnota"); }
-                            }
+                            carList[i].MileAge = CorrectMileAge();
                             SaveToFile();
                             break;
                         case 6:
                             Console.WriteLine("Ideš zmeniť cenu " + carList[i].Price + " na: ");
-
-                            while (correctValue)
-                            {
-                                if (!double.TryParse(Console.ReadLine(), out double prc))
-                                {
-                                    Console.WriteLine("Zadaj číselnú hodnotu");
-                                    continue;
-                                }
-                                if (prc > 0)
-                                {
-                                    carList[i].Price = prc;
-                                    correctValue = false;
-                                }
-                                else { Console.WriteLine("Cena musí byť minimále 1cent (0.01€)"); }
-                            }
+                            carList[i].Price = ReadCorrectDoubleValue();                           
                             SaveToFile();
                             break;
                         case 7:
                             Console.WriteLine("Ideš zmeniť počet dverí " + carList[i].Doors + " na:");
-                            carList[i].Doors =CorrectDoors ();
+                            carList[i].Doors = CorrectDoors();
                             SaveToFile();
                             break;
 
                         case 8:
                             Console.WriteLine("Ideš zmeniť mesto predaja " + carList[i].City + " na:");
-                            carList[i].City = Console.ReadLine();
+                            carList[i].City = CorrectString();
                             SaveToFile();
+                            break;
+                        case 9:
+                            Console.WriteLine("Ideš zmeniť stav auta z (havarované) " +carList[i].Condition +"na (A-áno, N-nie)");
+                            carList[i].Condition = CorrectCondition();
+                            SaveToFile();
+                            break;
+                        case 0:
                             break;
                     }
                     break;
                 }
             }
         }
-        public int CorrectDoors()
+        public int ReadCorrectIntValue()
+        {
+            bool isOk = false;
+            int ret = 0;
+            while (!isOk)
+            {
+                isOk = int.TryParse(Console.ReadLine(), out ret);
+                if (isOk)
+                {
+                    return ret;
+                }
+                Console.WriteLine("Zadaj číslo");
+            }
+            return ret;
+        }
+
+        public double ReadCorrectDoubleValue()
+        {
+            bool isOk = false;
+            double ret = 0;
+            while (!isOk)
+            {
+                isOk = double.TryParse(Console.ReadLine(), out ret);
+                if (isOk)
+                {
+                    return ret;
+                }
+                Console.WriteLine("Zadaj cenu");
+            }
+            return ret;
+        }
+        private string CorrectString()
+        {
+            while (true)
+            {
+                string correctStr = Console.ReadLine();
+                if (!string.IsNullOrEmpty(correctStr))
+                {
+                    return correctStr;
+                }
+                else
+                { Console.WriteLine("Je potrebné zadať text"); }
+            }
+        }
+
+        private int CorrectYear()
+        {
+            while (true)
+            {
+                int yOfProd = ReadCorrectIntValue();
+                if (yOfProd > 1870 && yOfProd <= DateTime.Now.Year)
+                {
+                    int correctYear = yOfProd;
+                    return correctYear;
+                }
+                else { Console.WriteLine("Rok musí byť od 1870 až po aktuálny rok"); }
+            }
+        }
+        private int CorrectMileAge()
+        {
+            while (true)
+            {
+                int mAge = ReadCorrectIntValue();
+                if (mAge >= 0)
+                {
+                    int correctmAge = mAge;
+                    return correctmAge;
+                }
+                else { Console.WriteLine("Kilometre nemôžu byť mínusová hodnota"); }
+            }
+        }
+        private int CorrectDoors()
         {
             while (true)
             {
@@ -212,53 +239,98 @@ namespace Appka1
                     return correctDoor;
                 }
                 else
-                {
-                    Console.WriteLine("Počet dverí môže byť 2,3,4 alebo 5");
-                }
+                { Console.WriteLine("Počet dverí môže byť 2,3,4 alebo 5"); }
             }
         }
 
-        bool correct = true;
-        public void AddCar()
+        private bool CorrectCondition()
+        {
+            while (true)
+            {
+                string value = Console.ReadLine().ToUpper();
+                if (!string.IsNullOrEmpty(value))
+                {
+                    if (value== "A")
+                    {
+                        return true;
+                    }
+                    if (value == "N")
+                    {
+                        return false;
+                    }
+                }
+                Console.WriteLine("Zadaj A-áno alebo N-nie");
+            }
+        }
+
+        private FuelType CorrectFuel()
         {
 
+            while (true)
+            {
+                string correctFuel = Console.ReadLine().ToUpper();
+                if (!string.IsNullOrEmpty(correctFuel))
+                {
+                    if (correctFuel=="B")
+                    {
+                        return FuelType.Benzin;
+                    }
+                    if (correctFuel == "N")
+                    {
+                        return FuelType.Nafta;
+                    }
+                    if (correctFuel == "E")
+                    {
+                        return FuelType.Elektricke;
+                    }
+                    if (correctFuel == "P")
+                    {
+                        return FuelType.Plyn;
+                    }
+                    if (correctFuel == "H")
+                    {
+                        return FuelType.Hybrid;
+                    }
+                }
+                Console.WriteLine("B-benzin, N-nafta, P-plyn, E-elektricke alebo H-hybrid"); 
+            }
+        }
 
 
+        //bool correct = true;
+        public void AddCar()
+        {
             Auto car = new Auto(GetNextId(carList));
 
             Console.WriteLine("Zadaj výrobcu");
-            car.Brand = Console.ReadLine();
+            car.Brand = CorrectString();
 
             Console.WriteLine("Zadaj typ auta");
-            car.TypeOfCar = Console.ReadLine();
+            car.TypeOfCar = CorrectString();
 
-            Console.Write("Zadaj typ paliva Benzin, Nafta, Plyn, Elektricke alebo Hybrid");
-            car.Fuel = (FuelType)Enum.Parse(typeof(FuelType), Console.ReadLine());
+            Console.WriteLine("Zadaj typ paliva B-benzin, N-nafta, P-plyn, E-elektricke alebo H-hybrid");
+            car.Fuel = CorrectFuel();
 
             Console.WriteLine("Zadaj rok výroby");
-            car.YearOfProd = ReadCorrectIntValue();
+            car.YearOfProd = CorrectYear();
 
             Console.WriteLine("Zadaj najazdené km");
-            car.MileAge = ReadCorrectIntValue();
-
+            car.MileAge = CorrectMileAge();
 
             Console.WriteLine("Zadaj cenu");
-            car.Price = double.Parse(Console.ReadLine());
+            car.Price = ReadCorrectDoubleValue();
 
-            Console.WriteLine("Zadaj počet dverí");            
+            Console.WriteLine("Zadaj počet dverí");
             car.Doors = CorrectDoors();
 
-            Console.WriteLine("Je auto havarované");
-            car.Condition = bool.Parse(Console.ReadLine());
+            Console.WriteLine("Je auto havarované A-áno, N-nie");
+            car.Condition = CorrectCondition();
 
             Console.WriteLine("Zadaj mesto predaja");
-            car.City = Console.ReadLine();
+            car.City = CorrectString();
 
             carList.Add(car);
             SaveToFile();
-
-
-
         }
 
         public void SaveToFile()
@@ -269,8 +341,5 @@ namespace Appka1
                 File.AppendAllText("bazos.txt", item.DescribeMe() + "\n");
             }
         }
-
-
-
     }
 }
--- Appka1/Program.cs
+++ Appka1/Program.cs
@@ -11,66 +11,53 @@ namespace Appka1
     {
         static void Main(string[] args)
         {
-            
+
 
             AutoBazar autoBazar = new AutoBazar();
             bool exit = true;
 
             while (exit)
             {
+                
                 Console.WriteLine("********************");
                 Console.WriteLine("1    -Pridaj auto");
                 Console.WriteLine("2    -Vypis zoznamu aut");
                 Console.WriteLine("3    -Vymaz auto");
                 Console.WriteLine("4    -Zmena parametrov");
+                Console.WriteLine("5    -Uloženie dát do súboru");
                 Console.WriteLine("0    -Vypni program");
                 Console.WriteLine("********************");
-
-                if (!int.TryParse(Console.ReadLine(), out int key))
-                {
-                    Console.WriteLine("Vyber spravnu možnosť");
-                    continue;
-                }
-
-
-                switch (key)
+                switch (autoBazar.ReadCorrectIntValue())
                 {
                     case 1:
-
+                        Console.Clear();
                         autoBazar.AddCar();
-                        
+                        Console.WriteLine("Auto bolo pridané");
                         break;
                     case 2:
-                        
+                        Console.Clear();
                         autoBazar.Vypis();
-                        
                         break;
                     case 3:
                         Console.WriteLine("Zadaj Id auta na odstránenie zo zoznamu");
-                        if (!int.TryParse(Console.ReadLine(), out int delCar))
-                        {
-                            Console.WriteLine("Zadaj číslo");
-                            continue;
-                        }
-
-                        autoBazar.DeleteCar(delCar);
-                        
+                        autoBazar.DeleteCar(autoBazar.ReadCorrectIntValue());
+                        Console.WriteLine("Auto bolo odstranene");
                         break;
                     case 4:
                         Console.WriteLine("Zadaj Id auta, ktoré chces upraviť");
-                        if (!int.TryParse(Console.ReadLine(), out int updCar))
-                        {
-                            Console.WriteLine("Zadaj číslo");
-                            continue;
-                        }
-                        autoBazar.UpdateCar(updCar);
+                        autoBazar.UpdateCar(autoBazar.ReadCorrectIntValue());
+                        Console.WriteLine("Auto bolo upravené");
+                        break;
+                    case 5:
+                        autoBazar.SaveToFile();
+                        Console.WriteLine("Data boli uložene do suboru");
                         break;
                     case 0:
                         exit = false;
                         break;
 
                 }
-               
+
             }
 
             Console.ReadLine();
